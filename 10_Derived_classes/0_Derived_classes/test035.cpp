b8deb85eff0e4ab154d9b663bb7dfd286d5ac2a2
KeiHasegawa <kei.hasegawa.18@gmail.com>
2019-05-09 16:08:15 +0900

    add test035

---
+++ 10_Derived_classes/0_Derived_classes/test035.cpp
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+struct A { int a; };
+struct B1 : virtual A { int b1; };
+struct B2 : virtual A { int b2; };
+struct C : B1, B2 { int c; };
+struct D : C { int d; };
+struct E1 : virtual D { int e1; };
+struct E2 : virtual D { int e2; };
+struct F : E1, E2 { int f; void g(); };
+
+void F::g()
+{
+  printf("F::g() called\n");
+  printf("a = %d\n", a);
+  printf("b1 = %d\n", b1);
+  printf("b2 = %d\n", b2);
+  printf("c = %d\n", c);
+  printf("d = %d\n", d);
+  printf("e1 = %d\n", e1);
+  printf("e2 = %d\n", e2);
+  printf("f = %d\n", f);
+
+  printf("E1::a = %d\n", E1::a);
+  printf("E2::a = %d\n", E2::a);
+  printf("B1::a = %d\n", B1::a);
+  printf("B2::a = %d\n", B2::a);
+  printf("E1::B1::a = %d\n", E1::B1::a);
+  printf("E1::B2::a = %d\n", E1::B2::a);
+  printf("E2::B1::a = %d\n", E2::B1::a);
+  printf("E2::B2::a = %d\n", E2::B2::a);
+}
+
+void h(F* pf)
+{
+  printf("h(F*) called\n");
+  printf("pf->a = %d\n", pf->a);
+  printf("pf->b1 = %d\n", pf->b1);
+  printf("pf->b2 = %d\n", pf->b2);
+  printf("pf->c = %d\n", pf->c);
+  printf("pf->d = %d\n", pf->d);
+  printf("pf->e1 = %d\n", pf->e1);
+  printf("pf->e2 = %d\n", pf->e2);
+  printf("pf->f = %d\n", pf->f);
+
+  printf("pf->E1::a = %d\n", pf->E1::a);
+  printf("pf->E2::a = %d\n", pf->E2::a);
+  printf("pf->B1::a = %d\n", pf->B1::a);
+  printf("pf->B2::a = %d\n", pf->B2::a);
+  printf("pf->E1::B1::a = %d\n", pf->E1::B1::a);
+  printf("pf->E1::B2::a = %d\n", pf->E1::B2::a);
+  printf("pf->E2::B1::a = %d\n", pf->E2::B1::a);
+  printf("pf->E2::B2::a = %d\n", pf->E2::B2::a);
+}
+
+int main()
+{
+  F x;
+  x.a = 1;
+  x.b1 = 2;
+  x.b2 = 3;
+  x.c = 4;
+  x.d = 5;
+  x.e1 = 6;
+  x.e2 = 7;
+  x.f = 8;
+
+  x.g();
+  h(&x);
+  return 0;
+}
+
+
+

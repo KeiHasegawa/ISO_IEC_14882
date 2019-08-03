c17b7906f771b716938396cacdd15b30a334098e
KeiHasegawa <kei.hasegawa.18@gmail.com>
2019-08-03 22:00:28 +0900

    update 000

--- 12_Special_member_functions/4_Destructors/1_ok/test000.cpp
+++ 12_Special_member_functions/4_Destructors/1_ok/test000.cpp
@@ -2,7 +2,7 @@
  * In an explicit destructor call, the destructor name appears as a . followed
  * by a type-name that names the destructor's class type. The invocation of a
  * destructor is subject to the usual rules for member functions (9.3), that
- * is, if the object is not of the destructorfs class type and not of a class
+ * is, if the object is not of the destructor' s class type and not of a class
  * derived from the destructor's class type, the program has undefined behavior
  * (except that invoking delete on a null pointer has no effect).
  */
@@ -21,9 +21,13 @@ typedef B B_alias;
 B* B_ptr = &D_object;
 void f() {
   D_object.B::~B(); // calls B's destructor
+  printf("\n");
   B_ptr->~B(); //calls D's destructor
+  printf("\n");
   B_ptr->~B_alias(); // calls D's destructor
+  printf("\n");
   B_ptr->B_alias::~B(); // calls B's destructor
+  printf("\n");
 }
 
 int main()

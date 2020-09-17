9bbff2dd4686e0a644d291904eb9a836b632d391
KeiHasegawa <kei.hasegawa.18@gmail.com>
2020-09-17 11:34:46 +0900

    up 163

--- 14_Templates/0_Templates/test163.cpp
+++ 14_Templates/0_Templates/test163.cpp
@@ -20,11 +20,10 @@ int main()
   S<char, int> sci;
   sci.pa = "abcdefg";
   S<int, char> sic;
-  sic.a = 2;
-  sic.b = '3';
+  sic.m = 2.0;
   S<int, float> sif;
   sif.a = 4;
   sif.b = 5.0;
-  printf("%d %s %d %c %d %f\n", si.m, sci.pa, sic.a, sic.b, sif.a, sif.b);
+  printf("%f %s %f %d %f\n", si.m, sci.pa, sic.m, sif.a, sif.b);
   return 0;
 }

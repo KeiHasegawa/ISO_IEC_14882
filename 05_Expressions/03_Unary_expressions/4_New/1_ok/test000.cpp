a45cc08010c52544a8594886a9fbd9b3e4c4de41
KeiHasegawa <kei.hasegawa.18@gmail.com>
2020-09-19 12:05:03 +0900

    up 000

--- 05_Expressions/03_Unary_expressions/4_New/1_ok/test000.cpp
+++ 05_Expressions/03_Unary_expressions/4_New/1_ok/test000.cpp
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
-int f(int sz)
+double f(int sz)
 {
   double* p = new double[sz];
-  int error = 0;
   for (int i = 0 ; i != sz ; ++i) {
-    if (p[i])
-      ++error;
+    p[i] = i;
   }
+  double sum = 0;
+  for (int i = 0 ; i != sz ; ++i)
+    sum += p[i];
   delete [] p;
-  return error;
+  return sum;
 }
 
 int main()
 {
-  if (int n = f(256))
-    printf("n = %d\n", n);
+  if (double d = f(256))
+    printf("d = %f\n", d);
   else
-    printf("ok\n");
+    printf("error\n");
   return 0;
 }

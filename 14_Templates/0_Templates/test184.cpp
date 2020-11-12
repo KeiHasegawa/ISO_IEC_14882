a18cf38dbcbd71441fffdf01a1429cdf7a028673
KeiHasegawa <kei.hasegawa.18@gmail.com>
2020-11-12 13:14:14 +0900

    add 184

---
+++ 14_Templates/0_Templates/test184.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+template<class C> struct S1 {
+  C m;
+  typedef typename C::X T;
+  void f(T n)
+  {
+    m += n;
+  }
+};
+
+template<class C2> void g(S1<C2>& sc, typename C2::X n)
+{
+  sc.f(n);
+}
+
+struct S2 {
+  typedef int X;
+  S2& operator+=(X n)
+  {
+    printf("S2::operator+=(X) called with %d\n", n);
+    return *this;
+  }
+};
+
+int main()
+{
+  S1<S2> x;
+  g(x, 5);
+  return 0;
+}

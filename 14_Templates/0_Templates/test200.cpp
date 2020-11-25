eca6e56fb51949e9a300e76baeccc4f03f12a778
KeiHasegawa <kei.hasegawa.18@gmail.com>
2020-11-25 09:54:51 +0900

    fix 200

--- 14_Templates/0_Templates/test200.cpp
+++ 14_Templates/0_Templates/test200.cpp
@@ -1,38 +1,36 @@
 #include <stdio.h>
 
-template<class A, class B, class C> struct S1 {
+template<class A, class B> struct S1 {
   A a;
   B b;
-  C c;
 };
 
 template<class C1> inline C1 f(C1 x)
 {
-  printf("x.a = %c x.b = %d x.c = %f\n", x.a, x.b, x.c);
+  printf("x.a = %c x.b = %d\n", x.a, x.b);
   return x;
 }
 
-template<class A, class B, class C, class D> D f(const S1<A,B,C>& x, D d)
+template<class A, class B, class D> D f(const S1<A,B>& x, D d)
 {
-  printf("x.a = %c x.b = %d x.c = %f d = %f\n", x.a, x.b, x.c, d);
+  printf("x.a = %c x.b = %d d = %f\n", x.a, x.b, d);
   return d;
 }
 
-template<class T, class A, class B, class C>
-S1<A,B,C> f(const S1<A,B,C>& x, T t)
+template<class T, class A, class B>
+S1<A,B> f(const S1<A,B>& x, T t)
 {
-  printf("x.a = %c x.b = %d x.c = %f t = %s\n", x.a, x.b, x.c, t);
+  printf("x.a = %c x.b = %d t = %s\n", x.a, x.b, t);
   return x;
 }
 
 int main()
 {
-  S1<char, int, double> x;
+  S1<char, int> x;
   x.a = '1';
   x.b = 2;
-  x.c = 3.0;
   f(x);
-  f<char, int, double, float>(x, 4.0);
-  f<const char*, char, int, double>(x, "foo");
+  f<char, int, float>(x, 4.0);
+  f<const char*, char, int>(x, "foo");
   return 0;
 }

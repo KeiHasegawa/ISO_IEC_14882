bec565fc418a73a6e6c0cd091e50b7f8ef85a744
KeiHasegawa <kei.hasegawa.18@gmail.com>
2020-12-16 12:47:01 +0900

    add 213

---
+++ 14_Templates/0_Templates/test213.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+template<class C1> struct S1 {
+
+  static const C1 V =  ( ((C1)(-1) < 0) ? - ( ((C1)(-1) < 0) ? (((((C1)1 << ( (sizeof(C1) * 8 - ((C1)(-1) < 0) ) - 1)) - 1) << 1) + 1) : ~(C1)0) - 1 : (C1)0) ;
+};
+
+template<class C2> struct S2 : S1<typename C2::T> {};
+
+struct X1 {
+  typedef unsigned char T;
+};
+
+struct X2 {
+  typedef char T;
+};
+
+struct X3 {
+  typedef unsigned short int T;
+};
+
+struct X4 {
+  typedef short int T;
+};
+
+int main()
+{
+  printf("%d\n", S2<X1>::V);
+  printf("%d\n", S2<X2>::V);
+  printf("%d\n", S2<X3>::V);
+  printf("%d\n", S2<X4>::V);
+  return 0;
+}

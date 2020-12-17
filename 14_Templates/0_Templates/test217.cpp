19e6376ba1f843155c63435dee1aebf2d8c8ca00
KeiHasegawa <kei.hasegawa.18@gmail.com>
2020-12-17 10:48:34 +0900

    add 217

---
+++ 14_Templates/0_Templates/test217.cpp
@@ -0,0 +1,27 @@
+extern "C" int printf(const char*, ...);
+
+struct S1 {};
+
+template<class> struct S2;
+
+template<class RandomAccessIterator>
+RandomAccessIterator
+f(RandomAccessIterator a, RandomAccessIterator b, S1)
+{
+  switch (b - a) {
+  default:
+    return b;
+  }
+}
+
+template<class Iterator>
+Iterator f(Iterator a, Iterator b)
+{
+  return f(a, b, S1());
+}
+
+int main()
+{
+  printf("f(0, 10) return value : %d\n", f(0, 10));
+  return 0;
+}

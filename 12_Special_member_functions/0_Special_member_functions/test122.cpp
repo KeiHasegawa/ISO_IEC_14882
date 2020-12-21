0aa731f1184ffc8d89d17f2cfd7d3bc4b4a98334
KeiHasegawa <kei.hasegawa.18@gmail.com>
2020-12-22 06:01:57 +0900

    add 122

---
+++ 12_Special_member_functions/0_Special_member_functions/test122.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct S {
+  int m;
+  S()
+  {
+    printf("S::S() called\n");
+  }
+  S(const S&) = default;
+  S& operator=(const S&) = default;
+};
+
+int main()
+{
+  S s;
+  s.m = 123;
+  S s2 = s;
+  printf("s2.m = %d\n", s2.m);
+  S s3;
+  s3 = s;
+  printf("s3.m = %d\n", s3.m);
+  return 0;
+}

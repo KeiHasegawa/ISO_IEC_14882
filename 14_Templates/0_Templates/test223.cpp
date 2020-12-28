bf55560114a5db8aaafeae9c00b9472703437215
KeiHasegawa <kei.hasegawa.18@gmail.com>
2020-12-28 16:40:43 +0900

    fix 223

--- 14_Templates/0_Templates/test223.cpp
+++ 14_Templates/0_Templates/test223.cpp
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 template<class C1, C1 v> struct S {
-  static constexpr C1 V = v;
+  static const C1 V = v;
 };
 
-template<class C2, C2 v> constexpr C2 S<C2, v>::V;
+template<class C2, C2 v> const C2 S<C2, v>::V;
 
 int main()
 {
   printf("S<int, 3>::V = %d\n", S<int, 3>::V);
   printf("S<char, 'c'>::V = %c\n", S<char, 'c'>::V);
+  printf("S<double, 4.0>::V = %f\n", S<double, 4.0>::V);
   return 0;
 }

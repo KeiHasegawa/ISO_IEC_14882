3e86bffb5ef1072037440496fdc20a328e55a445
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-01-09 07:47:12 +0900

    fix 238

--- 14_Templates/0_Templates/test238.cpp
+++ 14_Templates/0_Templates/test238.cpp
@@ -7,8 +7,6 @@ template<class C1, C1 V1> struct S1 {
 
 template<class C2> struct S2 : S1<bool, !bool(C2::V)> {};
 
-template<class C3> struct S3 : S2<C3>::T {};
-
 struct Y1 {
   static const bool V = false;
 };
@@ -19,12 +17,12 @@ struct Y2 {
 
 int main()
 {
-  S3<Y1> sy1;
+  S2<Y1> sy1;
   if (sy1.V)
     printf("ok\n");
   else
     printf("error\n");
-  if (S3<Y2>::V)
+  if (S2<Y2>::V)
     printf("error\n");
   else
     printf("ok\n");

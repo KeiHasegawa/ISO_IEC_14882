3d521c83ce83da2a9bd7410f5460407a78129951
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-01-19 11:09:04 +0900

    add 263

--- 14_Templates/0_Templates/test264.cpp
+++ 14_Templates/0_Templates/test264.cpp
@@ -6,6 +6,10 @@ template<class C1, int N = S1<C1>::V> struct S2 {
   C1 a[N];
 };
 
+template<class C2> struct S3 {
+  S2<C2> m;
+};
+
 template<class C2> struct S1 {
   static const int V = 10;
 };
@@ -13,11 +17,11 @@ template<class C2> struct S1 {
 int main()
 {
   typedef double X;
-  S2<X> s;
+  S3<X> s;
   for (int i = 0 ; i != S1<X>::V; ++i)
-    s.a[i] = S1<X>::V - i;
+    s.m.a[i] = S1<X>::V - i;
   for (int i = 0 ; i != S1<X>::V; ++i)
-    printf("%f ", s.a[i]);
+    printf("%f ", s.m.a[i]);
   printf("\n");
   return 0;
 }

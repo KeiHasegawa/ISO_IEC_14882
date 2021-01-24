d4ae3f0e0c575fea7a1f1eeb02dce57353d53793
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-01-24 15:33:45 +0900

    add 274

---
+++ 14_Templates/0_Templates/test274.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+template<bool V> struct S1 {
+  static constexpr bool V1 = V;
+};
+
+template<bool VV> struct S2 {
+  static const bool V2 = VV;
+};
+
+template<class C1> struct S3 {
+  S2<C1::V1> m1;
+};
+
+template<class C5, class C6>
+struct S5 : S3<S1<true>> {};
+
+template<class C7, bool> struct S6;
+
+template<class C8> struct S6<C8, true> {
+  S5<C8&, const C8&> m2;
+};
+
+int main()
+{
+  S6<int,true> s;
+  if (s.m2.m1.V2)
+    printf("ok\n");
+  else
+    printf("error\n");
+  return 0;
+}

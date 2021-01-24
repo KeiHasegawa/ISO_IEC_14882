a46290d9b117195fc4385e36d2be55f6698151be
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-01-25 05:23:28 +0900

    mv 275 -> 277 and add 275

--- 14_Templates/0_Templates/test277.cpp
+++ 14_Templates/0_Templates/test277.cpp
@@ -5,23 +5,25 @@ template<bool V> struct S1 {
 };
 
 template<bool VV> struct S2 {
-  static const bool V2 = !VV;
+  static const bool V2 = VV;
 };
 
 template<class C1> struct S3 {
   S2<C1::V1> m1;
 };
 
-struct S5 {
-  S3<S1<true>> x;
+template<class C7, bool> struct S6;
+
+template<class C8> struct S6<C8, true> {
+  S3<S1<true>> m2;
 };
 
 int main()
 {
-  S5 s5;
-  if (s5.x.m1.V2)
-    printf("error\n");
-  else
+  S6<int,true> s;
+  if (s.m2.m1.V2)
     printf("ok\n");
+  else
+    printf("error\n");
   return 0;
 }

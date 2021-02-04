db0756a25bfe26f4ae4c8448382456268a482db3
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-02-04 17:37:09 +0900

    fix 305

--- 14_Templates/0_Templates/test305.cpp
+++ 14_Templates/0_Templates/test305.cpp
@@ -8,11 +8,15 @@ template<class C1> struct S2 {
   static const bool V2 = false;
 };
 
-template<class C2> struct S3 {};
+template<class C2> struct S3 {
+  static const bool V3 = false;
+};
 
-template<class C3> struct S3<C3 const> {};
+template<class C3> struct S3<C3 const> {
+  static const bool V3 = true;
+};
 
-template<class C4> struct S4 : S1<S3<const C4>::V1> {};
+template<class C4> struct S4 : S1<S3<const C4>::V3> {};
 
 template<class C5, bool = S2<S4<C5>>::V2> struct S5 {
   struct T5 {

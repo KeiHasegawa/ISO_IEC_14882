1f7ff12ef6c0c4dc5a6fe653a5c7bb7ac9315c84
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-02-09 08:39:21 +0900

    add 314

--- 14_Templates/0_Templates/test314.cpp
+++ 14_Templates/0_Templates/test314.cpp
@@ -16,5 +16,9 @@ int main()
   printf("x.S1<short, int>::V1 = %d\n", x.S1<short, int>::V1);
   printf("x.S1<int>::V1 = %d\n", x.S1<int>::V1);
   printf("x.a = %f\n", x.a = 0.5);
+  S1<char, short> y;
+  printf("y.V1 = %d\n", y.V1);
+  printf("y.S1<short>::V1 = %d\n", y.S1<short>::V1);
+  printf("y.a = %f\n", y.a = 0.25);
   return 0;
 }

7608e55f28cc790755922f0ca3a0448bf64d9044
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-02-15 07:10:12 +0900

    add 345

--- 14_Templates/0_Templates/test346.cpp
+++ 14_Templates/0_Templates/test346.cpp
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-template<class C1> struct S1 {
-  static const int V1 = sizeof(C1);
+template<class> struct S1;
+
+template<class C1, int = S1<C1>::V1> struct S2;
+
+template<class C2> struct S1 {
+  static const int V1 = sizeof(C2);
 };
-
-template<class C2, int = S1<C2>::V1> struct S2;
-
 
 template<class C3, int N1> struct S2 {
   C3 a[N1];

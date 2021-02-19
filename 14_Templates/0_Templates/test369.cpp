faf0fbd781fc69c537814dd1c328ef5316809658
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-02-20 08:43:02 +0900

    fix 369

--- 14_Templates/0_Templates/test369.cpp
+++ 14_Templates/0_Templates/test369.cpp
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
-template<class...> struct S;
-
-template<class C> struct S<C> {
+template<class C> struct S {
   static C V;
 };
 
 template<class C> C S<C>::V;
 
+template<int N, class C> struct S<C[N]> {
+  static C V;
+};
+
+template<int N, class C> C S<C[N]>::V;
+
 int main()
 {
-  printf("S<double>::V = %f\n", S<double>::V = 1234);
+  printf("S<double>::V = %f, S<double[5]>::V = %f\n",
+	 S<double>::V = 1, S<double[5]>::V = 2);
   return 0;
 }
-

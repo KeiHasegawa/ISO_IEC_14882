acc63b5cff97eb5efce85788ba5376e89e93d403
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-02-23 10:42:40 +0900

    fix 385

--- 14_Templates/0_Templates/test385.cpp
+++ 14_Templates/0_Templates/test385.cpp
@@ -10,12 +10,24 @@ template<class C2> int* f(C2 (&a)[5], C2 (&b)[5])
   return 0;
 }
 
+template<class C5, class C6 = C5&> C6 g(int);
+
+template<class C7>
+auto h() noexcept -> decltype(g<C7>(0));
+
+
 template<class C3, class C4> struct S {
-  decltype(f(C3(), C4())) m;
+  decltype(f(h<C3>(), h<C4>())) m;
 };
 
 int main()
 {
-  printf("ok\n");
+  S<int,int> obj;
+  obj.m = 1234;
+  typedef int X[5];
+  S<X, X> obj2;
+  int a = 5678;
+  obj2.m = &a;
+  printf("obj.m = %d, *obj2.m = %d\n", obj.m, *obj2.m);
   return 0;
 }

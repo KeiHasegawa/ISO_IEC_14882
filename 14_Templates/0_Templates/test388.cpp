c2ba72075f101e611e945a9521ae504cb45a23be
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-02-24 08:48:55 +0900

    add 388

--- 14_Templates/0_Templates/test388.cpp
+++ 14_Templates/0_Templates/test388.cpp
@@ -1,23 +1,17 @@
 #include <stdio.h>
 
 template<class C1> struct S {
-  static typename C1::T1 f();
-  template<class C2> static void g(C2);
-  template<class C3, class = decltype(g<C3>(f()))> static void h(int);
+  template<class C2> static void f(int);
 };
 
-struct X {
-  typedef int T1;
-};
-
-template<class C4>
-template<class C5, class C6> void S<C4>::h(int a)
+template<class C1>
+template<class C2> void S<C1>::f(int a)
 {
-  printf("S<C4>::h(int) called with %d\n", a);
+  printf("S<C1>::f(int) called with %d\n", a);
 }
 
 int main()
 {
-  S<X>::h<int>(5);
+  S<char>::f<int>(5);
   return 0;
 }

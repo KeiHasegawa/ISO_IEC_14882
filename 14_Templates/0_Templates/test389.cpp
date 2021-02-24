d66c397232fca09301910c1e2f003d1a29196e35
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-02-24 09:37:54 +0900

    fix 389

--- 14_Templates/0_Templates/test389.cpp
+++ 14_Templates/0_Templates/test389.cpp
@@ -4,10 +4,10 @@ template<class C1> struct S {
   static int x;
 };
 
-template<class C2> int S<C2>::x;
-
 int main()
 {
   printf("S<char>::x = %d\n", S<char>::x = 1234);
   return 0;
 }
+
+template<class C2> int S<C2>::x;

ba23e3b4a01278ea1a8f2ad33c2bed8fa0d946af
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-02-28 11:39:19 +0900

    add 010

---
+++ 12_Special_member_functions/1_Constructors/1_ok/test010.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+struct S {
+  S();
+  S(const S&);
+  S(S&&);
+  S& operator=(const S&);
+  S& operator=(S&&);
+};
+
+S f(S a)
+{
+  return a;
+}
+
+int main()
+{
+  S x;
+  S y;
+  x = y;
+  x = f(y);
+  return 0;
+}
+
+S::S()
+{
+  printf("S::S() called\n");
+}
+
+S::S(const S&)
+{
+  printf("S::S(const S&) called\n");
+}
+
+S::S(S&&)
+{
+  printf("S::S(S&&) called\n");
+}
+
+S& S::operator=(const S&)
+{
+  printf("S::operator=(const S&) called\n");
+  return *this;
+}
+
+S& S::operator=(S&&)
+{
+  printf("S::opertor=(S&&) called\n");
+  return *this;
+}

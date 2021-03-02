a254c2d27b6375159921d8b774d5eb8117ae6291
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-03-03 06:17:02 +0900

    fix 402

--- 14_Templates/0_Templates/test402.cpp
+++ 14_Templates/0_Templates/test402.cpp
@@ -1,3 +1,25 @@
+/*
+ * 2021/03/03
+ * I could not judge if this program is ill-formed or not.
+ * 
+ * Think about the next code:
+ */
+#if 0
+struct X2 {};
+
+X2&& f(X2&& a)
+{
+  return a;
+}
+
+X2& g(X2& a)
+{
+  return f(a);
+}
+#endif
+/*
+ * If above code is ill-formed, bellow code is also ill-formed, I think.
+ */
 #include <stdio.h>
 
 template<class C> C&& f(C&& a)

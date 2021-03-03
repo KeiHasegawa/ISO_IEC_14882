ea011e3916f947ec6613a03b429f6f1e784c6101
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-03-03 17:12:36 +0900

    add 407

--- 14_Templates/0_Templates/test407.cpp
+++ 14_Templates/0_Templates/test407.cpp
@@ -1,57 +1,22 @@
 #include <stdio.h>
 
-template<bool V> struct S1 {
-  static const bool V1 = V;
-  typedef S1<V> T1;
-};
-
-template<bool, class C1> struct S2 { typedef C1 T2; };
-
-template <class C2> struct S3 { typedef C2 T3; };
-
-template<class C5, class C6 = C5&&> C6 f(int);
-
-template<class C7> auto g() -> decltype(f<C7>(0));
-
-template<class> struct S5 : S1<false> {};
-
-template<typename C3>
-auto g() -> decltype(f<C3>(0))
+template<typename C1> C1 f()
 {
-  return f<C3>(0);
 }
 
-struct S6 {
-  template<class C8, class C9> static
-  S3<decltype(g<C9>().*g<C8>())> sf(int);
-};
-
-template<class C10, class C11> struct S7 : S6 {
-  typedef decltype(sf<C10, C11>(0)) T7;
-};
-
-template<class, class> struct S8;
-
-template<class C12, class C13, class C14>
-struct S8<C12 C13::*, C14> {
-  typedef C12 C13::* MemPtr;
-  typedef typename S2<S5<C13>::V1, S7<MemPtr, C14>>::T2::T1 T8;
+template<class C2, class C3> struct S {
+  typedef C2 C3::* PM;
+  typedef decltype(f<PM>()) T;
 };
 
 struct X {
-  static const bool V1 = true;
-};
-
-template<> struct S7<double X::*, X> {
-  typedef int T1;
+  int m;
 };
 
 int main()
 {
-  S8<double X::*, X>::T8 x;
-  // C12 => double, C13 => X, C14 => X
-  // S5<X>::V1 => true
-  // S2<true, S7<MemPtr, X>>::T2 => S7<MemPtr, X>
-  printf("x = %d\n", x = 1234);
+  X x;
+  S<int, X>::T pm = &X::m;
+  printf("x.*pm = %d\n", x.*pm = 5789);
   return 0;
 }

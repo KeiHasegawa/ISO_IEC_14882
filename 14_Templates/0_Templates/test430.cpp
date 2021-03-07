0baa7f1633e1147ecb91c13188cce44ad2af5542
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-03-07 15:25:00 +0900

    fix 430

--- 14_Templates/0_Templates/test430.cpp
+++ 14_Templates/0_Templates/test430.cpp
@@ -1,33 +1,24 @@
 #include <stdio.h>
 
-template<class C1> inline void f(C1& a, C1& b);
+template<class C1> void f(C1 a)
+{
+  printf("a = %d\n", a);
+}
 
-template<class C2, int N> inline void f(C2 (&a)[N], C2 (&b)[N]);
-
-template<class C1> inline void f(C1& a, C1& b)
+template<class C2> void f(C2 a, C2 b)
 {
   printf("a = %f b = %f\n", a, b);
 }
 
-template<class C2, int N> inline void f(C2 (&a)[N], C2 (&b)[N])
-{
-  for (int i = 0 ; i != N ; ++i)
-    printf("%d %d ", a[i], b[i]);
-  printf("\n");
-}
-
 void f()
 {
-  double d = 1.0;
-  double d2 = -0.5;
-  f(d, d2);
-  int a[] = { 1, 2, 3 };
-  int b[] = { 4, 5, 6 };
-  f(a, b);
+  printf("`f()' called\n");
 }
 
 int main()
 {
+  f(1);
+  f(2.3, 4.5);
   f();
   return 0;
 }
